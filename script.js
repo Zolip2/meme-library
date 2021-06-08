5cdc798fc60de4dbc5fc5f77dfb1c4d8bd4abbae
Zolip2 <zoltanabraham3@gmail.com>
2021-06-08 11:29:07 +0200

    Rendszerezés

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#spongyabob").click(function(){
+    window.location.href = "spongyabob/index.html";
+})
+
+$("#nyelvtan").click(function(){
+    window.location.href = "nyelvtan/index.html";
+})
+
+$("#random").click(function(){
+    window.location.href = "random/index.html";
+})
